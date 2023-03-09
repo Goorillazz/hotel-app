c8295169a193f353b0c7200f6b892d2f691d9b1b
Christian Curilla <c.curilla@funk-gruppe.de>
2023-03-09 18:42:11 +0100

    Bookings sind da.

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Booking } from '../booking';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.component.html',
-  styleUrls: ['./bookings.component.css']
+  styleUrls: ['./bookings.component.css'],
 })
 export class BookingsComponent {
-
-booking = "Christians Buchung";
-
+  booking: Booking = {
+    id: 1,
+    name: 'Christian',
+    roomNumber: 100,
+    startDate: new Date(),
+    endDate: new Date('2021-07-21'),
+  };
 }

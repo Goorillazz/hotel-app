0a90548eb67d600064c447a5d592f95a46409527
Christian <chcurilla@googlemail.com>
2023-03-12 18:27:01 +0100

    Create Componente hinzu.

---
+++ src/app/create-booking/create-booking.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Booking } from '../booking';
+
+@Component({
+  selector: 'app-create-booking',
+  templateUrl: './create-booking.component.html',
+  styleUrls: ['./create-booking.component.css']
+})
+export class CreateBookingComponent {
+
+  booking: Booking = {
+    id: 100,
+    name: "Your Name",
+    roomNumber: 100,
+    startDate: new Date(),
+    endDate: new Date()
+  }
+
+}

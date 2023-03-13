75b83a7e470a835f6de20a6f0d4baccf424e265f
Christian Curilla <c.curilla@funk-gruppe.de>
2023-03-13 17:58:42 +0100

    Submit-Button ergänzt.

--- src/app/create-booking/create-booking.component.ts
+++ src/app/create-booking/create-booking.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { Booking } from '../booking';
+import { Bookings } from '../mock-bookings';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-booking',
   templateUrl: './create-booking.component.html',
-  styleUrls: ['./create-booking.component.css']
+  styleUrls: ['./create-booking.component.css'],
 })
 export class CreateBookingComponent {
 
+  constructor(private router: Router){}
+
   booking: Booking = {
     id: 100,
-    name: "Your Name",
+    name: 'Your Name',
     roomNumber: 100,
     startDate: new Date(),
-    endDate: new Date()
-  }
+    endDate: new Date(),
+  };
 
+  save(): void {
+    console.log('Make Booking');
+    Bookings.push(this.booking);
+    this.router.navigate(['bookings']);
+  }
 }
